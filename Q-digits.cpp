48bc1635b0fcf65bcca95e56ab168390ebe40a45
azrasadiabithi <azrasadiabithi@gmail.com>
2025-10-30 23:52:39 +0600

    Add files via upload

---
+++ Q-digits.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        long long N;
+        cin >> N;
+
+        if (N == 0) {
+            cout << 0 << endl;
+            continue;
+        }
+
+        while (N > 0) {
+            cout << N % 10;
+            N = N / 10;
+            if (N > 0) cout << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
